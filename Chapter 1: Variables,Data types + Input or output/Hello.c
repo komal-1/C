86fd681b23f0f52f3761ffb3aad90522d15b478f
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-02-15 22:02:59 +0530

    Update Hello.c

--- Chapter 1: Variables,Data types + Input or output/Hello.c
+++ Chapter 1: Variables,Data types + Input or output/Hello.c
@@ -1,1 +1,10 @@
+# include<stdio.h>
+  int main() {
+    printf("Hello World");
+    return 0;
+  }
 
+
+// for running any code in c , we have to open the terminal first and write 
+// gcc filename  ,gcc is a c- compiler.
+// and after this we have to write .a/.exe  .exe extention means it's an executable file.
