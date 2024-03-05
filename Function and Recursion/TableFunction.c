7f2f374801f90d22b06b801f64688aba39bc1b40
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-03-05 13:49:04 +0530

    Update TableFunction.c

--- Function and Recursion/TableFunction.c
+++ Function and Recursion/TableFunction.c
@@ -20,3 +20,26 @@ for(int i=1;i<=10;i++){
 }
 
 }
+
+
+OR,
+
+#include<stdio.h>
+
+void printTable(int n);
+
+int main(){
+    int n;
+    printf("enter number: ");
+    scanf("%d", &n);
+    printTable(n); // argument/actual parameter
+    return 0;
+}
+
+
+void printTable(int n){ //parameter/formal parameter
+for(int i=1;i<=10;i++){
+    printf("%d\n",i * n);
+}
+
+}
