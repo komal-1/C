6713066ac6ba576529dc2dcf2505407d53d1913c
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-03-11 22:36:34 +0530

    Update ArrayPointer.c

--- 7.ARRAY/ArrayPointer.c
+++ 7.ARRAY/ArrayPointer.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 int main(){
-    int age =22;
-    int *ptr = &age;
+    // int age =22;
+    // int *ptr = &age;
+
+    // float price = 20.00;
+    // float *ptr = &price;
+
+    char star = '*';
+    char *ptr = &star;
+    
+
+    
     printf("ptr = %u\n ",ptr);
     ptr ++;
     printf("ptr = %u\n ",ptr);
