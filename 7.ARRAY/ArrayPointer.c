5a70c559ad7f5ccb71d1bc24183d21d09a0402e1
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-03-11 22:24:11 +0530

    Update ArrayPointer.c

--- 7.ARRAY/ArrayPointer.c
+++ 7.ARRAY/ArrayPointer.c
@@ -2,8 +2,11 @@
 int main(){
     int age =22;
     int *ptr = &age;
-    printf("ptr = %u",ptr);
+    printf("ptr = %u\n ",ptr);
     ptr ++;
-    printf("ptr =%u",ptr);
+    printf("ptr = %u\n ",ptr);
+
+    ptr --;
+    printf("ptr = %u\n ",ptr);
     return 0;
 }
