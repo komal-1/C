e1612c8552d08a4c648b0d3fc849b7ac8c6beff2
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-03-11 21:50:31 +0530

    Update PQ41.c

--- 7.ARRAY/PQ41.c
+++ 7.ARRAY/PQ41.c
@@ -8,9 +8,9 @@ int main(){
     scanf("%f", &price[1]);
     scanf("%f", &price[2]);
 
-    printf("total price 1: %f",price[0] + (0.18*price[0]));
-    printf("total price 1: %f",price[1] + (0.18*price[1]));
-    printf("total price 1: %f",price[2] + (0.18*price[2]));
+    printf("total price 1: %f\n",price[0] + (0.18*price[0]));
+    printf("total price 1: %f\n",price[1] + (0.18*price[1]));
+    printf("total price 1: %f\n",price[2] + (0.18*price[2]));
 
     return 0;
 }
