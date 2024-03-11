2729a49d263b3f2d8bc26348e819a710b649f154
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-03-11 22:44:55 +0530

    Update ArrayPointer.c

--- 7.ARRAY/ArrayPointer.c
+++ 7.ARRAY/ArrayPointer.c
@@ -1,21 +1,40 @@
+// #include<stdio.h>
+// int main(){
+//     // int age =22;
+//     // int *ptr = &age;
+
+//     // float price = 20.00;
+//     // float *ptr = &price;
+
+//     char star = '*';
+//     char *ptr = &star;
+    
+
+    
+//     printf("ptr = %u\n ",ptr);
+//     ptr ++;
+//     printf("ptr = %u\n ",ptr);
+
+//     ptr --;
+//     printf("ptr = %u\n ",ptr);
+//     return 0;
+// }
+
+
+
+// we can also substract one pointer from another.
+// we can also compare 2 pointers.
+
 #include<stdio.h>
 int main(){
-    // int age =22;
-    // int *ptr = &age;
+    int age =22;
+    int _age = 23;
 
-    // float price = 20.00;
-    // float *ptr = &price;
+    int *ptr =&age;
+    int *_ptr =&_age;
 
-    char star = '*';
-    char *ptr = &star;
-    
-
-    
-    printf("ptr = %u\n ",ptr);
-    ptr ++;
-    printf("ptr = %u\n ",ptr);
-
-    ptr --;
-    printf("ptr = %u\n ",ptr);
+    printf("difference = %u\n", ptr - _ptr); // 1 means true
+    _ptr = &age;
+    printf("comparison = %u\n",ptr == _ptr); // 1 means true
     return 0;
 }
